0c3e3f9449e75f14dc60834b4b11f0b2867a5cb0
Phil Smart <philip.smart@jisc.ac.uk>
2022-06-29 22:59:55 +0100

    Add XSS vuln to items page

--- src/main/java/uk/ac/jisc/cybersec/controller/ItemController.java
+++ src/main/java/uk/ac/jisc/cybersec/controller/ItemController.java
@@ -1,11 +1,14 @@
 package uk.ac.jisc.cybersec.controller;
 
+import java.util.stream.Collectors;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import uk.ac.jisc.cybersec.repo.ItemRepository;
 
@@ -18,11 +21,18 @@ public class ItemController {
     @Autowired private ItemRepository itemRepo;
 
     @GetMapping("/items")
-    public String items(Model model) {
-        log.info("Getting items");
-        var items = itemRepo.findAll();
-        log.info("Found '{}' items", items.size());
-        model.addAttribute("items", items);
+    public String items(Model model, @RequestParam(required = false, name="q") final String searchTerm) {
+        log.info("Getting items, using search term {}",searchTerm);
+        var items = itemRepo.findAll();       
+        log.info("Found '{}' items", items.size());        
+        if (searchTerm != null) {
+            var itemsFiltered = 
+                    items.stream().filter(it -> it.getDescription().contains(searchTerm)).collect(Collectors.toList());
+            model.addAttribute("searchTerm", searchTerm);
+            model.addAttribute("items", itemsFiltered);
+        } else {
+            model.addAttribute("items", items);
+        }
         return "items";
     }   
 
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,4 @@
 INSERT INTO item(id, name, description) VALUES (1, 'Item One','A test item');
 INSERT INTO item(id, name, description) VALUES (2, 'Item Two','A test item two');
+INSERT INTO item(id, name, description) VALUES (3, 'Item Three','A test item three');
+INSERT INTO item(id, name, description) VALUES (4, 'Item Four','A test item four');
