4a7cd6f2524e20179fd6b6a4433211852eacb44d
Phil Smart <philip.smart@jisc.ac.uk>
2022-11-25 16:49:37 +0000

    Fix user-prefs

--- src/main/java/uk/ac/jisc/cybersec/controller/UserController.java
+++ src/main/java/uk/ac/jisc/cybersec/controller/UserController.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import uk.ac.jisc.cybersec.dto.UserAction;
 import uk.ac.jisc.cybersec.repo.UserRepository;
+import uk.ac.jisc.cybersec.secure.CustomUserDetails;
 
 /** Controller for the admin functions. */
 @Controller
@@ -43,10 +45,17 @@ public class UserController {
 
     @PostMapping("/user-prefs")
     @PreAuthorize("isAuthenticated()")
-    public String updateUsername(@ModelAttribute("userAction") final UserAction userAction) {
+    public String updateUsername(@ModelAttribute("userAction") final UserAction userAction, Authentication user) {
         log.debug("Updating [{}] to have username [{}]", userAction.getId(), userAction.getUsername());
         userRepo.updateUser(userAction.getUsername(), userAction.getId());
-        return "redirect:/user-admin";
+        
+        if (user.getPrincipal() instanceof CustomUserDetails) {
+            var userDetails = (CustomUserDetails) user.getPrincipal();
+            log.debug("Logged in user has id '{}'", userDetails.getUser().getId());
+            return "redirect:/user-prefs/"+userDetails.getUser().getId();
+        }
+        
+        return "redirect:/";
     }
 
 }
